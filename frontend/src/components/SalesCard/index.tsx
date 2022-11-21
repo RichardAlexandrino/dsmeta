52185d8ce869df902bf4bb08ac1b0dc5b57db0f6
Richard Alexandrino de Macedo <richardalexandrino@hotmail.com>
2022-11-20 21:25:41 -0300

    Send notification

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -16,8 +16,7 @@ function SalesCard() {
   const [maxDate, setMaxDate] = useState(max);
 
   const [sales, setSales] = useState<Sale[]>([]);
-
-
+  
   useEffect(() => {
 
     const dmin = minDate.toISOString().slice(0,10);
@@ -77,7 +76,7 @@ function SalesCard() {
                   <td>R$ {sale.amount.toFixed(2)}</td>
                   <td>
                     <div className="dsmeta-red-btn-container">
-                      <NotificationButton />
+                      <NotificationButton saleId={sale.id} />
                     </div>
                   </td>
                 </tr>
